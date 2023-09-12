c842074b2562daf22b4f820197151c6937b3853b
Baldomero <anders011097@gmail.com>
2023-09-12 16:32:00 -0400

    six commit

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-from flask import  Flask, render_template
+from flask import  Flask, render_template, url_for
 from datetime import datetime
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -17,16 +18,14 @@ def repeat (s,n):
 
 @app.route('/')
 def index ():
-
+    print (url_for('index'))
+    print (url_for('data'))
+    print (url_for('code', code = "Print()"))
     name = 'Anderson Angulo'
     friends = ["David", "Joe", "Adrian", "Daniel"]
     date = datetime.now()
 
-    return render_template(
-        'index.html', 
-        name = name, 
-        friends = friends, 
-        date = date)
+    return render_template('index.html', name = name, friends = friends, date = date)
 
 @app.route('/data')
 @app.route('/data/<name>')
@@ -43,4 +42,14 @@ def data (name = None, age = None, email = None):
 
     return render_template('data.html', data = my_data)
 
+@app.route('/code/<path:code>')
+def code (code):
+    code = escape(code) 
+    return render_template("code.html", code= code)
+
+
+@app.route('/login')
+def login():
+
+    return render_template("login.html")
     #Se recomienda dar saltos de lineas cuando son varios valores
